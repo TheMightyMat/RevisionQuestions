1e73aff5b247c739ba79d504a8b2167ccea6e252
Matthew Winfield <matthew.thomas.winfield@gmail.com>
2020-04-30 13:11:15 +0100

    Remove console log

--- static/js/markdown.js
+++ static/js/markdown.js
@@ -1,5 +1,4 @@
 function markdown() {
-	console.log("applying markdown")
 	let markdownElements = document.querySelectorAll('.markdown');
 	markdownElements.forEach(function (markdownElement) {
 		markdownElement.innerHTML = _.unescape(marked(markdownElement.innerHTML.trim()))
